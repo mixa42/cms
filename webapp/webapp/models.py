ffc5bf764dc0d13355caf21837399f5da15b9263
Tolkachev Michale <tolkachev.michale@gmail.com>
2023-02-28 10:19:32 +0700

    model_test

--- webapp/webapp/models.py
+++ webapp/webapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+import datetime
 
 CHOICES_VISIBLE = [
     ('1', _('Yes')),
@@ -10,18 +11,16 @@ class Menu(models.Model):
     name = models.CharField('Название', max_length=100)
     url = models.CharField('Ссылка', max_length=255)
     position = models.PositiveIntegerField('Позиция', default=1)
+
     visible = models.CharField(
         max_length = 2,
-        verbose_name = _('visible'),
+        verbose_name = _('Видимость'),
         choices = CHOICES_VISIBLE,
         default = CHOICES_VISIBLE[0][0],
     )
-    status = models.CharField(
-        max_length=24,
-        default='published'
-    )
-   
-    #parent = models.ForeignKey('Menu', on_delete=models.PROTECT)
+    
+    timestamp = models.DateTimeField(default=datetime.datetime.now())
+    parent = models.ForeignKey('Menu', on_delete=models.SET_NULL, blank=True, null=True, default=None, verbose_name='Родительский элемент')
 
     def __str__(self):
         return str(self.name)
