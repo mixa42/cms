c57624292179a3e3bab0d63b024a130c793b4f95
Tolkachev Michale <tolkachev.michale@gmail.com>
2023-02-28 16:17:06 +0700

    Реализовать через while

--- webapp/webapp/templatetags/common_tags.py
+++ webapp/webapp/templatetags/common_tags.py
@@ -3,9 +3,13 @@ from webapp.models import Menu
 register = template.Library()
 
 
+def tree_struct(menu_items):
+    return menu_items
+
+
 @register.inclusion_tag('menu.html', takes_context=True)
 def show_top_menu(context):
-    menu_items = Menu.objects.all()
+    menu_items = tree_struct(Menu.objects.filter(visible=1))
     return {
         "menu_items": menu_items,
     }
